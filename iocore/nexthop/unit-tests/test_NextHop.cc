e28a148457a9e8fc3cb31ac06ab18e256da5daa8
Aaron Canary <acanary@oath.com>
2018-02-20 16:49:45 -0600

    NextHop.WIP3

---
+++ iocore/nexthop/unit-tests/test_NextHop.cc
@@ -0,0 +1,42 @@
+/** @file
+  Test file for NextHop namespace
+  @section license License
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+#include "catch.hpp"
+
+#include "NextHopHost.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// ======= test for string_view ========
+// test cases:
+//[constructor] [operator] [type] [access] [capacity] [modifier] [operation] [compare] [find]
+
+TEST_CASE("NextHop Host constructor", "[NextHop] [Host] [constructor]")
+{
+  SECTION("Literal look for NULL")
+  {
+    ts::string_view sv("hello");
+    REQUIRE(sv.size() == 5);
+    REQUIRE(sv.length() == 5);
+    REQUIRE(sv.empty() == false);
+    REQUIRE(sv == "hello");
+  }
+}
